2b4c0e3f81d1d601c6cf00917b69483175cef327
Shriyansh Kumar <71579575+Shriyansh7870@users.noreply.github.com>
2023-10-14 12:07:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// Include the route files with correct case
+
 const contactRoutes = require("./Route/contact");
 const technologyRoutes = require("./Route/Technology");
 const foodRoutes = require("./Route/Food");
 const expressRoutes = require("./Route/express");
 
-// Mount the route files on the main app
+
 app.use("/api/main", contactRoutes);
 app.use("/api/main", technologyRoutes);
 app.use("/api/main", foodRoutes);
